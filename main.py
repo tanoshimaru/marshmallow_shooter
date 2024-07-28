73e89ad9034da7230f00773314f51880cf51b967
tanoshimaru <p42f2fza@s.okayama-u.ac.jp>
2024-07-28 19:37:48 +0900

    update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 from mic import MIC
 from pwm import PWM
-import time
 
 
 Mic = MIC()
@@ -16,9 +15,9 @@ def main():
             if Mic.get_vad():
                 doa = Mic.get_doa()
                 print(doa)
-                if 0 < doa <= 180:
+                if 5 <= doa <= 180:
                     Motor.turn_right(speed)
-                elif 180 < doa < 360:
+                elif 180 < doa <= 355:
                     Motor.turn_left(speed)
                 else:
                     Motor.stop()

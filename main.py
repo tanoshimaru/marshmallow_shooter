efddf0dbdc2150b89f8be506fb0979a51483743d
tanoshimaru <p42f2fza@s.okayama-u.ac.jp>
2024-08-08 00:38:40 +0900

    update

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ def main():
             print(doa)
             if -180 <= doa <= -10:
                 Motor.turn_right(80)
+                counter = 0
             elif 10 <= doa <= 180:
                 Motor.turn_left(80)
+                counter = 0
             else:
                 counter += 1
             if 10 <= abs(doa) <= 50:
@@ -27,7 +29,7 @@ def main():
             elif 90 < abs(doa) <= 180:
                 time.sleep(0.3)
         Motor.stop()
-        time.sleep(0.3)
+        time.sleep(0.5)
     print("Marshmallow-Shoot!")
     Servo.servo_ctrl(0)
 
